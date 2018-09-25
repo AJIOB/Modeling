7cafed2bf34b98007e4bb8d460a340e7ead32ed2
AJIOB <aleksandr9809@gmail.com>
2018-09-25 20:35:35 +0300

    Random inplementation was added

---
+++ lab4/AJIOB_random.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define RANDOM_M    313107
+#define RANDOM_A    134279
+#define RANDOM_R0   1
+
+double generate_random();
--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+#include "AJIOB_random.h"
+
 int main()
 {
-    printf("hello, world\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%lf\n", generate_random());
+    }
+
     return 0;
 }
