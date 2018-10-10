0a5695ef49e391553127338bbd0194bf2908265a
AJIOB <aleksandr9809@gmail.com>
2018-10-10 09:25:21 +0300

    Config updating & config checking fix

--- lab6/config.h
+++ lab6/config.h
@@ -1,12 +1,21 @@
 #pragma once
 
-#define MODEL_MAX_TIMEOUT   2
-#define MODEL_MAX_QUEUE     1
-#define MODEL_PI_1          0.8
-#define MODEL_PI_2          0.6
+#include <stdint.h>
+
+#define MODEL_SOURCE_LAMBDA             (2.5)
+#define MODEL_MAX_QUEUE                 (-1)
+#define MODEL_CHANNEL_MU                (3.0)
+#define MODEL_ADDITIONAL_MU_ORDER       (100)
 
 #define RANDOM_M    313107
 #define RANDOM_A    134279
 #define RANDOM_R0   1
 
-#define MAX_ITERATIONS 100000
+#define MAX_ITERATION_PERIODS 100000
+
+// auto config
+#define MODEL_ADDITIONAL_MU             (((MODEL_SOURCE_LAMBDA) + (MODEL_CHANNEL_MU)) * (MODEL_ADDITIONAL_MU_ORDER))
+#define MODEL_DELTA_T                   (1.0 / (MODEL_ADDITIONAL_MU + MODEL_SOURCE_LAMBDA + MODEL_CHANNEL_MU))
+#define MODEL_SOURCE_RO                 (1 - ((MODEL_DELTA_T) * (MODEL_SOURCE_LAMBDA)))
+#define MODEL_PI_1                      (1 - ((MODEL_DELTA_T) * (MODEL_CHANNEL_MU)))
+#define MAX_ITERATIONS                  ((int64_t) ((MAX_ITERATION_PERIODS) / (MODEL_DELTA_T)))
--- lab6/main.cpp
+++ lab6/main.cpp
@@ -3,8 +3,42 @@
 #include "config.h"
 #include "model.h"
 
+int check_config()
+{
+    if ((MODEL_SOURCE_RO) < 0)
+    {
+        fprintf(stderr, "MODEL_SOURCE_RO < 0: %lf\n", (MODEL_SOURCE_RO));
+        return 1;
+    }
+
+    if ((MODEL_SOURCE_RO) > 1)
+    {
+        fprintf(stderr, "MODEL_SOURCE_RO > 1: %lf\n", (MODEL_SOURCE_RO));
+        return 1;
+    }
+    
+    if ((MODEL_PI_1) < 0)
+    {
+        fprintf(stderr, "MODEL_PI_1 < 0: %lf\n", (MODEL_PI_1));
+        return 1;
+    }
+    
+    if ((MODEL_PI_1) > 1)
+    {
+        fprintf(stderr, "MODEL_PI_1 > 1: %lf\n", (MODEL_PI_1));
+        return 1;
+    }
+
+    return 0;
+}
+
 int main()
 {
+    if (check_config())
+    {
+        return 1;
+    }
+
     Model_t::static_model_info.reset();
 
     for (int i = 0; i < MAX_ITERATIONS; i++)
